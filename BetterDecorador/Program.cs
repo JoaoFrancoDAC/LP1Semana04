34245ecc8caf0809827291ee70bae0bb1fbe6c75
JoaoFrancoDAC <jvfdc15@gmail.com>
2025-03-13 21:00:29 +0000

    added solution to the project

--- BetterDecorador/Program.cs
+++ BetterDecorador/Program.cs
@@ -6,7 +6,28 @@ namespace BetterDecorador
     {
         private static void Main(string[] args)
         {
-            Console.WriteLine("Hello LP1!");
+            string frase = args[0];
+            char c = Convert.ToChar(args[1]);
+            int i = Convert.ToInt32(args[2]);
+            Console.WriteLine(Decor(frase, c, i));
+        }
+
+
+        private static string Decor(string s, char dec, int numero)
+        {
+            char decorador = dec;
+            string character = decorador.ToString();
+            string frase = "";
+            for (int i = 0; i < numero; i ++)
+            {
+                frase += character;
+            }
+            frase = frase +" "+ s + " ";
+            for (int i = 0; i < numero; i ++)
+            {
+                frase += character;
+            }
+            return frase;
         }
     }
 }
